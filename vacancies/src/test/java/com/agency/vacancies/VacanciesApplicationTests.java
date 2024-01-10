4a8603df353bf33045e4441cec4e3cd097e3f692
pouliose <vaggpouli@gmail.com>
2024-01-10 17:53:23 +0200

    initial commit

---
+++ vacancies/src/test/java/com/agency/vacancies/VacanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency.vacancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
